57c5fb0db2bb2fe27c6a1fba4d4229f59bfc87c9
ahmedaliawan09 <ahmedawan81151@gmail.com>
2024-05-07 21:34:14 +0500

    chnages app.js

--- Client/joblet/src/App.js
+++ Client/joblet/src/App.js
@@ -1,49 +1,111 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Jobs from './pages/Jobs';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+import { createContext, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Grid, makeStyles } from "@material-ui/core";
 
-const App = () => {
-  const [backendData, setBackendData] = useState({ users: [] });
+import Welcome, { ErrorPage } from "./component/Welcome";
+import Navbar from "./component/Navbar";
+import Login from "./component/Login";
+import Logout from "./component/Logout";
+import Signup from "./component/Signup";
+import Home from "./component/Home";
+import Applications from "./component/Applications";
+import Profile from "./component/Profile";
+import CreateJobs from "./component/recruiter/CreateJobs";
+import MyJobs from "./component/recruiter/MyJobs";
+import JobApplications from "./component/recruiter/JobApplications";
+import AcceptedApplicants from "./component/recruiter/AcceptedApplicants";
+import RecruiterProfile from "./component/recruiter/Profile";
+import MessagePopup from "./lib/MessagePopup";
+import isAuth, { userType } from "./lib/isAuth";
 
-  useEffect(() => {
-    fetch("/api")
-      .then(response => response.json())
-      .then(data => {
-        setBackendData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+const useStyles = makeStyles((theme) => ({
+  body: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "98vh",
+    paddingTop: "64px",
+    boxSizing: "border-box",
+    width: "100%",
+  },
+}));
 
+export const SetPopupContext = createContext();
+
+function App() {
+  const classes = useStyles();
+  const [popup, setPopup] = useState({
+    open: false,
+    severity: "",
+    message: "",
+  });
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
-
-      <div>
-        {(backendData.users.length === 0) ? (
-          <p>Loading data from backend</p>
-        ) : (
-          <>
-            <h2>Data fetched from backend just for testing</h2>
-            {backendData.users.map((user, i) => (
-              <p key={i}>{user}</p>
-            ))}
-          </>
-        )}
-      </div>
-    </>
+    <BrowserRouter>
+      <SetPopupContext.Provider value={setPopup}>
+        <Grid container direction="column">
+          <Grid item xs>
+            <Navbar />
+          </Grid>
+          <Grid item className={classes.body}>
+            <Switch>
+              <Route exact path="/">
+                <Welcome />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+              <Route exact path="/logout">
+                <Logout />
+              </Route>
+              <Route exact path="/home">
+                <Home />
+              </Route>
+              <Route exact path="/applications">
+                <Applications />
+              </Route>
+              <Route exact path="/profile">
+                {userType() === "recruiter" ? (
+                  <RecruiterProfile />
+                ) : (
+                  <Profile />
+                )}
+              </Route>
+              <Route exact path="/addjob">
+                <CreateJobs />
+              </Route>
+              <Route exact path="/myjobs">
+                <MyJobs />
+              </Route>
+              <Route exact path="/job/applications/:jobId">
+                <JobApplications />
+              </Route>
+              <Route exact path="/employees">
+                <AcceptedApplicants />
+              </Route>
+              <Route>
+                <ErrorPage />
+              </Route>
+            </Switch>
+          </Grid>
+        </Grid>
+        <MessagePopup
+          open={popup.open}
+          setOpen={(status) =>
+            setPopup({
+              ...popup,
+              open: status,
+            })
+          }
+          severity={popup.severity}
+          message={popup.message}
+        />
+      </SetPopupContext.Provider>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
